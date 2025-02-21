f7a121d0cecb2e4e789fcb44f5a657616f831de1
Josh Rimes - Desktop <joshjohn1104@gmail.com>
2025-02-21 12:52:00 +0000

    Started work on map file upload

--- data_app/app.py
+++ data_app/app.py
@@ -7,20 +7,27 @@ app = Dash()
 
 app.layout = [
     html.H1(children='Interactive Functional Region Map', style={'textAlign':'center'}),
-    html.Label(children='Upload POI data:', id='file_upload_label', style={'textAlign':'centre'}),
-    dcc.Upload(id='file_input', children=html.Button('Upload')),
-    html.Div(id='data_output')
+    html.Label(children='Upload POI data:', id='POI_upload_label', style={'textAlign':'centre'}),
+    dcc.Upload(id='POI_file_input', children=html.Button('Upload')),
+    html.Hr(),
+    html.Label(children='Upload Map (.tif):', id='map_upload_label', style={'textAlign':'centre'}),
+    dcc.Upload(id='map_file_input', children=html.Button('Upload')),
+    html.Hr(),
+    html.Div(id='data_output'),
 ]
 
 @callback(
     Output('data_output', 'children'),
-    Input('file_input', 'contents'),
-    State('file_input', 'filename')
+    [Input('POI_file_input', 'contents'),
+     Input('map_file_input', 'contents')],
+    State('POI_file_input', 'filename'),
+    
 )
 
 def update_output(contents, filename):
     if contents is not None:
         df = data_handling.parse_contents(contents, filename)
+        df = data_handling.clean_POI_data(df)
         children = [data_handling.data_table(df, filename)]
         return children
 
