e04c07ef10994cccd463c1904af19435e185e358
Josh Rimes - Desktop <joshjohn1104@gmail.com>
2025-03-25 01:39:29 +0000

    Added styling to the app for visual clarity

--- data_app/app.py
+++ data_app/app.py
@@ -6,26 +6,35 @@ import map_handling
 app = Dash()
 
 app.layout = [
-    html.H1(children='Interactive Functional Region Map', style={'textAlign':'center'}),
-    html.Label(children='Upload POI data:', id='POI_upload_label', style={'textAlign':'centre'}),
-    dcc.Upload(id='POI_file_input', children=html.Button('Upload')), # Opens pop-up for upload when clicked
-    html.Hr(), # Horizontal line
-    # TODO: See how much customisation can be done to: the cluster sizes, hiding POIs
+    html.H1(children='Interactive Functional Region Map'),
     html.Div([
-        dcc.Checklist(id = 'checklist', options = [
-            {'label': 'Hide clusters', 'value': 'clusters'},
-        ],
-        value = [], # Default checklist has Hide clusters unticked
-        inline = True),
-        html.Hr(),
-        html.Label(children='Select which clusters are visible:', id='dropdown_label', style={'textAlign':'centre', 'margin-bottom':'5px'}),
-        dcc.Dropdown(['All', 'Accommodation, eating and drinking', 'Commercial services', 'Attractions', 'Sport and entertainment', 'Education and health', 'Public Infrastructure', 'Manufacturing and production', 'Retail', 'Transport'], 'All', multi=True, id='dropdown'),
-        html.Hr(),
-        html.Label(children='Size of clusters:', id='slider_label', style={'textAlign':'centre', 'margin-bottom':'5px'}),
-        dcc.Slider(0.0005, 0.005, 0.0005, value=0.001, id='slider')
-    ]),
-    html.Hr(),
-    html.Div(id='data_output'), # Displays the data table
+        dcc.Upload(id='POI_file_input', children=html.Button('Choose file / Drag & drop', className='download-button'))# Opens pop-up for upload when clicked
+    ], className='download-button-div'),
+
+    # TODO: Change minimum POIs for clusters
+    html.Div([
+        html.Div([
+            dcc.Checklist(id = 'checklist', options = [
+            {'label': 'Hide clusters', 'value': 'clusters'},],
+            value = [], # Default checklist has Hide clusters unticked
+            inline = True),
+        ], className='checklist-div'),
+
+        html.Div([
+            html.Label(children='Select which clusters are visible:', id='dropdown_label', style={'textAlign':'centre', 'margin-bottom':'5px'}),
+            dcc.Dropdown(['All', 'Accommodation, eating and drinking', 'Commercial services', 'Attractions', 'Sport and entertainment', 'Education and health', 'Public Infrastructure', 'Manufacturing and production', 'Retail', 'Transport'], 'All', multi=True, id='dropdown'),
+        ], className='dropdown-div'),
+        
+        html.Div([
+            html.Label(children='Size of clusters:', id='slider_label', style={'textAlign':'centre', 'margin-bottom':'5px'}),
+            dcc.Slider(0.0005, 0.005, 0.0005, value=0.001, marks=None, id='slider', className='slider',
+                       tooltip={'placement': 'bottom', 'always_visible': True}),
+        ], className='slider-div'),
+    ], className='toolbar-div'),
+
+    html.Div(id='data_output', className='data-div'), # Displays the data table
+
+    html.H2(children='Made by Josh Rimes    2025'),
 ]
 
 @callback(
